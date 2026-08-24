d8e1f33f31560162b51414901dc4394652e4b2cb
vwkxviu <vivikakovganukvad@gmail.com>
2026-08-24 23:17:58 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 const w = Math.floor(Math.random()*100)+1;
-
+let attems =  0;
 
 const num = document.querySelector(".num");
 const guess = document.querySelector(".guess");
-const result = document.querySelector(".result")
+const result = document.querySelector(".result");
+const pop = document.querySelector(".pop");
 guess.addEventListener("click", function(){
         const q = Number(num.value);
         num.value = "";
         if(q > w){
             result.textContent = "Меньше нафик"
+            attems++
+            pop.textContent = `Попыток: ${attems}`
         }else if(q < w){
             result.textContent = "Больше нафик"
+            attems++;
+            pop.textContent = `Попыток: ${attems}`
         }else{
             result.textContent = "Угадал сука"
+            pop.textContent = `Красава, справился за ${attems} попыток`
         }
 })
+
